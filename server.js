ff7da11d941c34ffa9b4cedce1c0cd45b96f9079
Lydia S <149545941+LISims88@users.noreply.github.com>
2024-06-10 17:43:10 -0500

    deploy TS

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config()
 const express = require("express");
 const knex = require("knex");
-const db = require("../db"); 
+const db = require("./db"); 
 const PORT = process.env.PORT || 4000;
 const cors = require("cors")
 const app = express();
